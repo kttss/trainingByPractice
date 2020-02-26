829df41f1a72c60cef9b51b0eef9c14f0045fd27
kttss <kattouss.issam@gmail.com>
2020-02-26 13:23:29 +0100

    add my files

---
+++ dist/assets/js/main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var SCP = SCP || {};
+
+$(document).ready(function () {
+
+	SCP.test.init();
+	SCP.test2.init();
+
+});
+
+SCP.test = {
+	init: function () {
+			void 0
+	}
+};
+
+SCP.test2 = {
+	init: function () {
+
+	}
+};
