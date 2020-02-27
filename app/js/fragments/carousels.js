866865eca1b4a3afb9fd828a8a4be70ea49f0955
salmane <salmanechougrani@gmail.com>
2020-02-27 09:24:27 +0100

    carousel

--- app/js/fragments/carousels.js
+++ app/js/fragments/carousels.js
@@ -6,7 +6,8 @@ SCP.carousels = {
 		var $carouselSlider = $(".lame-carousel.owl-carousel");
 		$carouselSlider.owlCarousel({
 			items: 1,
-			nav: true
+			nav: true,
+			navText: ["<span class='icon icon-back'></span>", "<span class='icon icon-next'></span>"],
 		});
 	}
 };
