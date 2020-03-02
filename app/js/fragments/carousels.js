6b8ee139f7b8e0339c4b3426e989e152633c49fc
youssef samih <youssef.samih97@gmail.com>
2020-03-02 09:14:48 +0100

    finish testimonial section

--- app/js/fragments/carousels.js
+++ app/js/fragments/carousels.js
@@ -4,7 +4,9 @@
 SCP.carousels = {
 	init: function () {
 		var $carouselSlider = $(".lame-carousel.owl-carousel"),
-			$carouselTimer = $(".lame-timeline__inner.owl-carousel");
+			$carouselTimer = $(".lame-timeline__inner.owl-carousel"),
+			$carouselTestimonialTimer = $(".lame-testimonials_background_main_slider.owl-carousel"),
+			$carouselTestimonial = $('.lame-testimonials_background_main_slider');
 
 		$carouselSlider.owlCarousel({
 			items: 1,
@@ -23,9 +25,33 @@ SCP.carousels = {
 		$carouselTimer.on("changed.owl.carousel", function (e) {
 			var index = e.page.index;
 			$(this).find(".owl-dot.active").index();
-			console.log($(this).find(".owl-dot.active").prevAll());
+			$(this).find(".owl-dot.active").prevAll();
 			$(this).find(".owl-dot.active").prevAll().addClass("before-dot");
-			console.log($(this).find(".owl-dot.active").nextAll());
+			$(this).find(".owl-dot.active").nextAll();
+			$(this).find(".owl-dot.active").removeClass("before-dot");
+			$(this).find(".owl-dot.active").nextAll().removeClass("before-dot");
+
+			if (index == 3) {
+				$(this).find(".owl-dot.active").addClass("last-before");
+			} else {
+				$(this).find(".owl-dot").removeClass("last-before");
+			}
+		});
+
+		$carouselTestimonial.owlCarousel({
+			items: 1,
+			nav: true,
+			navText: ["<span class='icon icon-left-arrow'></span>", "<span class='icon icon-right'></span>"],
+			dots: true,
+			smartSpeed: 1000
+		});
+
+		$carouselTestimonialTimer.on("changed.owl.carousel", function (e) {
+			var index = e.page.index;
+			$(this).find(".owl-dot.active").index();
+			$(this).find(".owl-dot.active").prevAll();
+			$(this).find(".owl-dot.active").prevAll().addClass("before-dot");
+			$(this).find(".owl-dot.active").nextAll();
 			$(this).find(".owl-dot.active").removeClass("before-dot");
 			$(this).find(".owl-dot.active").nextAll().removeClass("before-dot");
 
